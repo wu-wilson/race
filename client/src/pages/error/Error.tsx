36b21c9477eade7614f29479f95a56c81ea87895
wu-wilson <wilson.cui.wu@gmail.com>
2022-10-16 20:52:38 -0400

    added page not found

--- client/src/pages/error/Error.tsx
+++ client/src/pages/error/Error.tsx
@@ -1,5 +1,37 @@
+import { RiAlertLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Particles from "../../components/particles/Particles";
+import vars from "../../index.module.scss";
+import styles from "./Error.module.scss";
+
 const Error = () => {
-  return <div>404 Error Page Not Found</div>;
+  const navigate = useNavigate();
+
+  const dashboardRedirect = () => {
+    navigate("/dashboard");
+  };
+
+  return (
+    <>
+      <Particles num={40} radius={6} color={vars["primary_color"]} />
+      <div className={styles["container"]}>
+        <div className={styles["card"]}>
+          <div className={styles["title"]}>
+            <RiAlertLine className={styles["icon"]} /> 404
+          </div>
+          <div className={styles["subtitle"]}>Oops! Something's missing...</div>
+          <div className={styles["text"]}>
+            We can't seem to find the page you're looking for. The page doesn't
+            exist, isn't available, or loaded incorrectly.
+          </div>
+          <button className={styles["button"]} onClick={dashboardRedirect}>
+            Back to Home
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Error;
