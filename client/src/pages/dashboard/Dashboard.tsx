63e12154811eb0f88ca15989757fed12cecf495b
wu-wilson <wilson.cui.wu@gmail.com>
2022-10-24 02:03:27 -0400

    added temporary logout button

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,22 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../../context/firebase";
+import { useNavigate } from "react-router-dom";
 import styles from "./Dashboard.module.scss";
 
 const Dashboard = () => {
-  return <div className={styles["container"]}>dashboard</div>;
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await signOut(auth).then(() => {
+      navigate("/login");
+    });
+  };
+
+  return (
+    <div className={styles["container"]}>
+      dashboard<button onClick={logout}>Sign Out</button>
+    </div>
+  );
 };
 
 export default Dashboard;
