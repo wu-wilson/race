d0b7615550364cf2a8308ac6cba06d6aeb6b37cd
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-01 12:41:31 -0400

    added persisting state to dark mode

--- client/src/context/ThemeContext.tsx
+++ client/src/context/ThemeContext.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 
 export type setTheme = {
-  darkMode: boolean;
+  darkMode: boolean | null;
   setDarkMode: (val: boolean) => void;
 };
 
@@ -9,7 +15,22 @@ export type setTheme = {
 const ThemeContext = createContext<setTheme | null>(null);
 
 export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const storedDark = localStorage.getItem("dark_mode");
+    if (storedDark) {
+      setDarkMode(JSON.parse(storedDark));
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode != null) {
+      localStorage.setItem("dark_mode", JSON.stringify(darkMode));
+    }
+  }, [darkMode]);
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
