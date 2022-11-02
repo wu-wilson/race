bd51a0723b96882fae4e73c603e9bb96538af635
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-02 13:49:30 -0400

    refactored navbar to be relatively positioned

--- client/src/pages/dashboard/make-reservation/MakeReservation.tsx
+++ client/src/pages/dashboard/make-reservation/MakeReservation.tsx
@@ -5,15 +5,42 @@ import styles from "./MakeReservation.module.scss";
 const progressDescriptions = ["Date", "Time", "Party", "Review"];
 
 const MakeReservation = () => {
-  const [step, setStep] = useState<number>(3);
+  const [step, setStep] = useState<number>(1);
+
+  const prevStep = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const nextStep = () => {
+    if (step < progressDescriptions.length) {
+      setStep(step + 1);
+    }
+  };
+
+  const submit = () => {};
 
   return (
     <div className={styles["container"]}>
-      <StepProgressBar
-        numSteps={4}
-        step={step}
-        descriptions={progressDescriptions}
-      />
+      <div className={styles["progress"]}>
+        <StepProgressBar
+          numSteps={progressDescriptions.length}
+          step={step}
+          descriptions={progressDescriptions}
+        />
+      </div>
+      <div className={styles["cal-container"]}>Test</div>
+      <div className={styles["buttons"]}>
+        <button onClick={prevStep} disabled={step === 1}>
+          Prev
+        </button>
+        {step === progressDescriptions.length ? (
+          <button onClick={submit}>Submit</button>
+        ) : (
+          <button onClick={nextStep}>Next</button>
+        )}
+      </div>
     </div>
   );
 };
