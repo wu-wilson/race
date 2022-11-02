b1d4e377400cb294186c5774ac3d949a9fb81ce7
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-02 12:39:54 -0400

    made progress bar responsive to font-size, added descrptions

--- client/src/components/step-progress-bar/StepProgressBar.tsx
+++ client/src/components/step-progress-bar/StepProgressBar.tsx
@@ -3,9 +3,11 @@ import styles from "./StepProgressBar.module.scss";
 const StepProgressBar = ({
   numSteps,
   step,
+  descriptions,
 }: {
   numSteps: number;
   step: number;
+  descriptions: string[];
 }) => {
   return (
     <div className={styles["container"]}>
@@ -14,20 +16,23 @@ const StepProgressBar = ({
         className={styles["progress"]}
         style={{ width: `${((step - 1) / (numSteps - 1)) * 100}%` }}
       />
-      {Array.from({ length: numSteps }, (_, index) => index + 1).map((num) => (
-        <div
-          className={`${styles["circle"]} ${
-            num < step || num > numSteps
-              ? styles["complete"]
-              : num === step
-              ? styles["active"]
-              : styles["inactive"]
-          }`}
-          key={num}
-        >
-          {num}
-        </div>
-      ))}
+      {Array.from({ length: numSteps }, (_, index) => index + 1).map(
+        (num, index) => (
+          <div
+            className={`${styles["circle"]} ${
+              num < step || num > numSteps
+                ? styles["complete"]
+                : num === step
+                ? styles["active"]
+                : styles["inactive"]
+            }`}
+            key={num}
+          >
+            {num}
+            <span className={styles["description"]}>{descriptions[index]}</span>
+          </div>
+        )
+      )}
     </div>
   );
 };
--- client/src/pages/dashboard/make-reservation/MakeReservation.tsx
+++ client/src/pages/dashboard/make-reservation/MakeReservation.tsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import StepProgressBar from "../../../components/step-progress-bar/StepProgressBar";
 import styles from "./MakeReservation.module.scss";
 
+const progressDescriptions = ["Date", "Time", "Party", "Review"];
+
 const MakeReservation = () => {
   const [step, setStep] = useState<number>(3);
 
   return (
     <div className={styles["container"]}>
-      <StepProgressBar numSteps={3} step={step} />
+      <StepProgressBar
+        numSteps={4}
+        step={step}
+        descriptions={progressDescriptions}
+      />
     </div>
   );
 };
