c8fbda0a9a8117295dd82fa106622b4a1ed7165f
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-01 22:37:16 -0400

    set up skeleton for progress bar

---
+++ client/src/components/step-progress-bar/StepProgressBar.tsx
@@ -0,0 +1,7 @@
+import styles from "./StepProgressBar.module.scss";
+
+const StepProgressBar = () => {
+  return <div>Step Progress Bar</div>;
+};
+
+export default StepProgressBar;
--- client/src/pages/dashboard/make-reservation/MakeReservation.tsx
+++ client/src/pages/dashboard/make-reservation/MakeReservation.tsx
@@ -1,7 +1,12 @@
+import StepProgressBar from "../../../components/step-progress-bar/StepProgressBar";
 import styles from "./MakeReservation.module.scss";
 
 const MakeReservation = () => {
-  return <div className={styles["container"]}>Make Reservation</div>;
+  return (
+    <div className={styles["container"]}>
+      <StepProgressBar />
+    </div>
+  );
 };
 
 export default MakeReservation;
