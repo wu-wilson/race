b0bb6dde8581a147c6f0ba4ac4a1dc74bf7baad2
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-01 21:59:57 -0400

    added responsive tab system to dashboard

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,61 @@
-import { signOut } from "firebase/auth";
-import { auth } from "../../context/firebase";
-import { useNavigate } from "react-router-dom";
+import { ReactElement, useState } from "react";
+import {
+  FaBasketballBall,
+  FaCalendarCheck,
+  FaClock,
+  FaUserFriends,
+} from "react-icons/fa";
 import Navbar from "../../components/navbar/Navbar";
 import styles from "./Dashboard.module.scss";
 
-const Dashboard = () => {
-  const navigate = useNavigate();
+type tab = {
+  name: string;
+  icon: ReactElement;
+};
 
-  const logout = async () => {
-    await signOut(auth).then(() => {
-      navigate("/login");
-    });
-  };
+const tabs: tab[] = [
+  {
+    name: "My Bookings",
+    icon: <FaClock className={styles["tab-icon"]} size={15} />,
+  },
+  {
+    name: "Make Reservation",
+    icon: <FaCalendarCheck className={styles["tab-icon"]} size={15} />,
+  },
+  {
+    name: "Court Status",
+    icon: <FaBasketballBall className={styles["tab-icon"]} size={15} />,
+  },
+  {
+    name: "Friend Activity",
+    icon: <FaUserFriends className={styles["tab-icon"]} size={15} />,
+  },
+];
+
+const Dashboard = () => {
+  const [selectedTab, setSelectedTab] = useState<string>(tabs[0].name);
 
   return (
     <>
       <Navbar />
       <div className={styles["container"]}>
-        dashboard<button onClick={logout}>Sign Out</button>
+        <div className={styles["card"]}>
+          <div className={styles["tabs"]}>
+            {tabs.map((tab) => (
+              <div
+                className={`${styles["tab"]} ${
+                  selectedTab === tab.name ? styles["selectedTab"] : ""
+                }`}
+                key={tab.name}
+                onClick={() => setSelectedTab(tab.name)}
+              >
+                {tab.icon}
+                {tab.name}
+              </div>
+            ))}
+          </div>
+          <div className={styles["screen"]}></div>
+        </div>
       </div>
     </>
   );
