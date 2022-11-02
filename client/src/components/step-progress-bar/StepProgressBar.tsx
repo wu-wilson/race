141e875e4cd696ea842cba28f07570f1543b35e8
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-01 23:03:26 -0400

    started step-progress-bar

--- client/src/components/step-progress-bar/StepProgressBar.tsx
+++ client/src/components/step-progress-bar/StepProgressBar.tsx
@@ -1,7 +1,23 @@
 import styles from "./StepProgressBar.module.scss";
 
-const StepProgressBar = () => {
-  return <div>Step Progress Bar</div>;
+const StepProgressBar = ({
+  numSteps,
+  step,
+  setStep,
+}: {
+  numSteps: number;
+  step: number;
+  setStep: (step: number) => void;
+}) => {
+  return (
+    <div className={styles["container"]}>
+      {Array.from({ length: numSteps }, (_, index) => index + 1).map((step) => (
+        <div className={styles["circle"]} key={step}>
+          {step}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default StepProgressBar;
--- client/src/pages/dashboard/make-reservation/MakeReservation.tsx
+++ client/src/pages/dashboard/make-reservation/MakeReservation.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import StepProgressBar from "../../../components/step-progress-bar/StepProgressBar";
 import styles from "./MakeReservation.module.scss";
 
 const MakeReservation = () => {
+  const [step, setStep] = useState<number>(0);
+
   return (
     <div className={styles["container"]}>
-      <StepProgressBar />
+      <StepProgressBar numSteps={3} step={step} setStep={setStep} />
     </div>
   );
 };
