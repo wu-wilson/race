0248cc50f3003557ab0c2bbccfec9ef7650d868c
wu-wilson <wilson.cui.wu@gmail.com>
2022-11-28 22:10:37 -0500

    changed mouse events to Pointer Events

--- client/src/components/time-selector/TimeSelector.tsx
+++ client/src/components/time-selector/TimeSelector.tsx
@@ -100,9 +100,9 @@ const TimeSelector = ({
               ? styles["fifteen"]
               : styles["thirty-fortyfive"]
           } ${index === slots.length - 1 ? styles["bottom"] : ""}`}
-          onMouseDown={(e) => handleMouseDown(e, slot)}
-          onMouseUp={() => handleMouseUp(slot)}
-          onMouseEnter={(e) => handleHover(e, slot)}
+          onPointerDown={(e) => handleMouseDown(e, slot)}
+          onPointerUp={() => handleMouseUp(slot)}
+          onPointerEnter={(e) => handleHover(e, slot)}
         >
           {index % 4 === 0 ? (
             <span className={styles["time-label"]}>
