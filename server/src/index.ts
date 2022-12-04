652fb47ad3f7b7c10507b2bc1e3c6be00f054e0e
wu-wilson <wilson.cui.wu@gmail.com>
2022-12-04 17:17:02 -0500

    changed mysql2 connection to pooling

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,110 +11,116 @@ const app = express();
 app.use(cors());
 app.use(bodyParser());
 
-// Connect to database
-const database = mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+// Create connection pool
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  connectionLimit: 20,
 });
 
-database.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("connected to database...");
-    }
+// Check pool connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database connected");
+    connection.release();
+  }
 });
 
 // Get reservations for a specific court on a given day
-app.get("/booked/:courtType/:courtNum/:day/:month/:year", (req: Request, res: Response) => {
+app.get(
+  "/booked/:courtType/:courtNum/:day/:month/:year",
+  (req: Request, res: Response) => {
     try {
-        const QUERY = `SELECT * FROM reservation WHERE courtType = "${req.params.courtType}" AND courtNum = "${req.params.courtNum}" AND date = "${req.params.day} ${req.params.month} ${req.params.year}"`;
-        database.query(QUERY, (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.send(err);
-            } else {
-                return res.send(result);
-            }
-        });
+      const QUERY = `SELECT * FROM reservation WHERE courtType = "${req.params.courtType}" AND courtNum = "${req.params.courtNum}" AND date = "${req.params.day} ${req.params.month} ${req.params.year}"`;
+      pool.query(QUERY, (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.send(err);
+        } else {
+          return res.send(result);
+        }
+      });
     } catch (err) {
-        console.error(err);
-        return res.send(err);
+      console.error(err);
+      return res.send(err);
     }
-});
+  }
+);
 
 // Get bookings for a user
 app.get("/bookings/:user", (req: Request, res: Response) => {
-    const QUERY = `SELECT * FROM reservation WHERE uid = ?`;
-    database.query(QUERY, [req.params.user], (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `SELECT * FROM reservation WHERE uid = ?`;
+  pool.query(QUERY, [req.params.user], (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 // Make a reservation
 app.post("/reserve", (req: Request, res: Response) => {
-    const QUERY = `INSERT INTO reservation (uid, courtType, courtNum, date, start, end) VALUES ("${req.body.uid}", "${req.body.courtType}", "${req.body.courtNum}", "${req.body.date}", "${req.body.start}", "${req.body.end}")`;
-    database.query(QUERY, (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `INSERT INTO reservation (uid, courtType, courtNum, date, start, end) VALUES ("${req.body.uid}", "${req.body.courtType}", "${req.body.courtNum}", "${req.body.date}", "${req.body.start}", "${req.body.end}")`;
+  pool.query(QUERY, (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 // Delete a specific reservation for a user
 app.delete("/delete/reservation/:user", (req: Request, res: Response) => {
-    const QUERY = `DELETE FROM reservation WHERE uid = ? AND date = "${req.body.date}" AND start = "${req.body.start}" AND end = "${req.body.end}" AND courtType = "${req.body.courtType}" AND courtNum = "${req.body.courtNum}"`;
-    database.query(QUERY, [req.params.user], (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `DELETE FROM reservation WHERE uid = ? AND date = "${req.body.date}" AND start = "${req.body.start}" AND end = "${req.body.end}" AND courtType = "${req.body.courtType}" AND courtNum = "${req.body.courtNum}"`;
+  pool.query(QUERY, [req.params.user], (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 // Check into a court
 app.post("/check-in", (req: Request, res: Response) => {
-    const QUERY = `INSERT INTO courtStatus (courtType, courtNum) VALUES ("${req.body.courtType}", "${req.body.courtNum}")`;
-    database.query(QUERY, (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `INSERT INTO courtStatus (courtType, courtNum) VALUES ("${req.body.courtType}", "${req.body.courtNum}")`;
+  pool.query(QUERY, (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 // Check out of a court
 app.delete("/check-out", (req: Request, res: Response) => {
-    const QUERY = `DELETE FROM courtStatus WHERE courtType = "${req.body.courtType}" AND courtNum = "${req.body.courtNum}" LIMIT 1`;
-    database.query(QUERY, (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `DELETE FROM courtStatus WHERE courtType = "${req.body.courtType}" AND courtNum = "${req.body.courtNum}" LIMIT 1`;
+  pool.query(QUERY, (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 // Get the number of people checked into a court
 app.get("/count/:courtType/:courtNum", (req: Request, res: Response) => {
-    const QUERY = `SELECT COUNT(*) AS num_people FROM courtStatus WHERE courtType = "${req.params.courtType}" AND courtNum = "${req.params.courtNum}"`;
-    database.query(QUERY, (err, result) => {
-        if (err) {
-            return res.send(err);
-        } else {
-            return res.send(result);
-        }
-    });
+  const QUERY = `SELECT COUNT(*) AS num_people FROM courtStatus WHERE courtType = "${req.params.courtType}" AND courtNum = "${req.params.courtNum}"`;
+  pool.query(QUERY, (err, result) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.send(result);
+    }
+  });
 });
 
 export default app;
